190b2be48258eb3b865c8a25129c86c216f08812
martucrespi <127051398+martucrespi@users.noreply.github.com>
2024-09-10 01:56:02 -0300

    Update splines_cheb_f2.py

--- f2/splines_cheb_f2.py
+++ f2/splines_cheb_f2.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 from mpl_toolkits.mplot3d import Axes3D
 
-# Definición de la función f2
 def f2(x1, x2):
     term1 = 0.75 * np.exp(-((9 * x1 - 2) ** 2 / 4) - ((9 * x2 - 2) ** 2 / 4))
     term2 = 0.75 * np.exp(-((9 * x1 + 1) ** 2 / 49) - ((9 * x2 + 1) ** 2 / 10))
@@ -11,12 +10,10 @@ def f2(x1, x2):
     term4 = -0.2 * np.exp(-((9 * x1 - 7) ** 2 / 4) - ((9 * x2 - 3) ** 2 / 4))
     return term1 + term2 + term3 + term4
 
-# Intervalo de evaluación
 x = np.linspace(-1, 1, 100)
 y = np.linspace(-1, 1, 100)
 X, Y = np.meshgrid(x, y)
 
-# Generación de puntos de grilla usando nodos de Chebyshev
 def generate_chebyshev_grid_data(n_points):
     chebyshev_points = np.cos(np.pi * (2 * np.arange(n_points) + 1) / (2 * n_points))
     x = np.sort(chebyshev_points)
@@ -25,7 +22,6 @@ def generate_chebyshev_grid_data(n_points):
     Z = f2(X, Y)
     return X, Y, Z
 
-# Función para interpolar y graficar en 3D
 def interpolate_and_plot_3d_chebyshev(n_points):
     Xg, Yg, Zg = generate_chebyshev_grid_data(n_points)
     points = np.vstack((Xg.ravel(), Yg.ravel())).T
@@ -41,7 +37,6 @@ def interpolate_and_plot_3d_chebyshev(n_points):
     ax.set_zlabel('f2(x1, x2)')
     plt.show()
 
-# Visualización de gráficos separados
 interpolate_and_plot_3d_chebyshev(5)
 interpolate_and_plot_3d_chebyshev(10)
 interpolate_and_plot_3d_chebyshev(50)
