d4fe5474763941698009440f86c187678ace0b0c
martucrespi <127051398+martucrespi@users.noreply.github.com>
2024-09-10 01:55:09 -0300

    Update splines_f2.py

--- f2/splines_f2.py
+++ f2/splines_f2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 from mpl_toolkits.mplot3d import Axes3D
 
-# Definición de la función f2
+
 def f2(x1, x2):
     term1 = 0.75 * np.exp(-((9 * x1 - 2) ** 2 / 4) - ((9 * x2 - 2) ** 2 / 4))
     term2 = 0.75 * np.exp(-((9 * x1 + 1) ** 2 / 49) - ((9 * x2 + 1) ** 2 / 10))
@@ -11,12 +11,12 @@ def f2(x1, x2):
     term4 = -0.2 * np.exp(-((9 * x1 - 7) ** 2 / 4) - ((9 * x2 - 3) ** 2 / 4))
     return term1 + term2 + term3 + term4
 
-# Intervalo de evaluación
+
 x = np.linspace(-1, 1, 100)
 y = np.linspace(-1, 1, 100)
 X, Y = np.meshgrid(x, y)
 
-# Generación de puntos de grilla para interpolación
+
 def generate_grid_data(n_points):
     x = np.linspace(-1, 1, n_points)
     y = np.linspace(-1, 1, n_points)
@@ -24,7 +24,7 @@ def generate_grid_data(n_points):
     Z = f2(X, Y)
     return X, Y, Z
 
-# Función para interpolar y graficar en 3D
+
 def interpolate_and_plot_3d(n_points):
     Xg, Yg, Zg = generate_grid_data(n_points)
     points = np.vstack((Xg.ravel(), Yg.ravel())).T
@@ -40,7 +40,7 @@ def interpolate_and_plot_3d(n_points):
     ax.set_zlabel('f2(x1, x2)')
     plt.show()
 
-# Visualización de gráficos separados
+
 interpolate_and_plot_3d(5)
 interpolate_and_plot_3d(10)
 interpolate_and_plot_3d(50)
